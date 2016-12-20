0f96a82cdc1b9bf018fda7afd16b447ac882deb3
Peter <peter.binkowski@gmail.com>
2016-12-19 22:59:05 -0600

    new logo

--- source/site/snippets/menu.php
+++ source/site/snippets/menu.php
@@ -1,14 +1,15 @@
 <header>
     <div class="container">
     <div class="title">
-        <svg width="45" height="60" viewBox="0 0 164 194" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                <g id="pb" stroke="#fff" stroke-width="10">
-                    <path d="M77.9044126,94.1601233 L0.631991083,47.418102 L78.577558,0.268895643 L78.577558,94.1601233 L78.577558,188.051351 L0.631991083,140.902145 L77.9044126,94.1601233 Z M157.577558,47.418102 L79.6319911,94.5673083 L79.6319911,0.268895643 L157.577558,47.418102 Z"></path>
-                </g>
+    <a href="/">
+        <svg width="152px" height="204px" viewBox="154 47 152 204" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="Group" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(162.000000, 54.000000)">
+                <path d="M102.611944,14.3880559 C118.637335,14.3880559 148.859991,82.140009 148.859991,82.140009 L55.6119441,82.140009 C49.2603816,82.140009 86.5865535,14.3880559 102.611944,14.3880559 Z" id="Triangle" stroke="#fff" stroke-width="15" transform="translate(101.875977, 48.264032) rotate(90.000000) translate(-101.875977, -48.264032) "></path>
+                <path d="M34.7687946,14.2312054 C50.7941852,14.2312054 81.0168415,81.9831585 81.0168415,81.9831585 L-12.2312054,81.9831585 C-20.0710491,81.9831585 18.743404,14.2312054 34.7687946,14.2312054 Z" id="Triangle" stroke="#fff" stroke-width="15" transform="translate(33.875977, 48.107182) scale(-1, 1) rotate(90.000000) translate(-33.875977, -48.107182) "></path>
+                <path d="M35.9833862,106.016614 C52.0087769,106.016614 87.9716675,164.594739 82.2314331,173.768567 C76.4911987,182.942395 -11.0166138,173.768567 -11.0166138,173.768567 C-11.0166138,173.768567 19.9579956,106.016614 35.9833862,106.016614 Z" id="Triangle" stroke="#fff" stroke-width="15" transform="translate(35.914605, 141.931219) scale(-1, 1) rotate(90.000000) translate(-35.914605, -141.931219) "></path>
             </g>
         </svg>
-        <h3 class="name"><a href="/">Peter Binkowski • Designer | Developer</a></h3>
+    </a>
     </div>
     <nav id="menu">
         <ul>
